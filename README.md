d305ae54a4bc4899123f31180bb7635002b79110
Aj163 <a3sajs@gmail.com>
2017-12-02 09:51:53 +0530

    Todo App

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Todo-App
-A simple To-do list app using NODE.js
+- A simple To-do list app using NODE.js
+- Connects to mlab (MongoDB) to store todo list items
+
+![Alt text](./website_screenshot.png?raw=true "Screenshot")
--- To-do App/controllers/todoController.js
+++ To-do App/controllers/todoController.js
@@ -1,26 +1,45 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [{item: 'Get milk'}, {item: 'Codeforces'}, {item: 'Todo App'}];
+//Connect to the database
+//mongoose.connect('mongodb://test:test@ds125896.mlab.com:25896/todo_app');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://test:test@ds125896.mlab.com:25896/todo_app', { keepAlive: true, reconnectTries: Number.MAX_VALUE, useMongoClient: true });
+
+//Create a schema - like a blueprint
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+//var data = [{item: 'Get milk'}, {item: 'Codeforces'}, {item: 'Todo App'}];
 
 module.exports = function(app)
 {
 	app.get('/todo', function(req, res)
 	{
-		res.render('todo', {todos: data});
+		//Get data from mongodb and pass it to view
+		Todo.find({}, function(err, data)
+		{
+			if (err) throw err;
+			res.render('todo', {todos: data});
+		});
 	});
 
 	app.post('/todo', urlencodedParser, function(req, res)
 	{
-		data.push(req.body);
-		res.json(data);
+		var newTodo = Todo(req.body).save(function(err, data){
+			if (err) throw err;
+			res.json(data);
+		});	
 	});
 
 	app.delete('/todo/:item', function(req, res)
 	{
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g, '-') !== req.params.item;
+		Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+			if (err) throw err;
+			res.json(data);
 		});
-		res.json(data);
 	});
 };
